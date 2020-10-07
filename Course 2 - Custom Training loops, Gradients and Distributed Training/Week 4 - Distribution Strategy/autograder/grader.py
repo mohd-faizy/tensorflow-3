629b19a7bb4a21f49bfd8a5df8a62ba2097e179b
andres-zartab <andrezb5@gmail.com>
2020-10-07 11:56:00 -0500

    C2 grader W4 - add grader files

---
+++ Course 2 - Custom Training loops, Gradients and Distributed Training/Week 4 - Distribution Strategy/autograder/grader.py
@@ -0,0 +1,9 @@
+
+import compiled
+import learner_mod
+import solution_mod
+from disable_warnings import *
+import inspect
+from nb_parser import get_graded_cells
+
+graded_cells = get_graded_cells()
