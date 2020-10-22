c199407e76f67b5aff5385e2164ff2f1f17164e6
andres-zartab <andrezb5@gmail.com>
2020-10-22 11:56:48 -0500

    grader C3 W1 - refactor send_feedback to handle errors

--- Course 3 - Advanced Computer Vision/Week 1 - Concepts/autograder/tools.py
+++ Course 3 - Advanced Computer Vision/Week 1 - Concepts/autograder/tools.py
@@ -1,10 +1,9 @@
 import sys
 import json
 
-def send_feedback(score, msg):
+def send_feedback(score, msg, err=False):
     post = {"fractionalScore": score, "feedback": msg}
     print(json.dumps(post))
+    if error:
+        print(str(msg), file=sys.stderr)
     exit()
-
-def print_stderr(error_msg):
-    print(str(error_msg), file=sys.stderr)
