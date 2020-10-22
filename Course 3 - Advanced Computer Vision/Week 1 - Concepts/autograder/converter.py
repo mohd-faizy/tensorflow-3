824d8fbe94061a91a3063a9087369593708266cb
andres-zartab <andrezb5@gmail.com>
2020-10-22 11:56:27 -0500

    grader C3 W1 - add support for local dev

--- Course 3 - Advanced Computer Vision/Week 1 - Concepts/autograder/converter.py
+++ Course 3 - Advanced Computer Vision/Week 1 - Concepts/autograder/converter.py
@@ -3,20 +3,22 @@ import shutil
 from zipfile import ZipFile
 from tools import send_feedback
 
-
 submission_dir = "/shared/submission/"
+sub_destination = "/grader/mymodel.zip"
 
+if os.getenv("LOCALGRADER"):
+    submission_dir = "./submission/"
+    sub_destination = "./mymodel.zip"
+
+learner_file = None
 for file in os.listdir(submission_dir):
-    if file.endswith('.zip'):
+    if file.endswith(".zip"):
         learner_file = file
-    else:
-        learner_file = None
 
 if learner_file is None:
-    send_feedback(0.0, "No .zip was found in the submission directory.")
+    send_feedback(0.0, "No .zip was found in the submission directory.", err=True)
 
 sub_source = submission_dir + learner_file
-sub_destination = '/grader/mymodel.zip'
 shutil.copyfile(sub_source, sub_destination)
 
 saved_model_path = "./mymodel.zip"
