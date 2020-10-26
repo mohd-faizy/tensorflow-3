1609b005f063077bf451431a00c128df7ce02836
andres-zartab <andrezb5@gmail.com>
2020-10-26 16:37:15 -0500

    grader C3 W1 - updated requirements.txt and Dockerfile

--- Course 3 - Advanced Computer Vision/Week 1 - Concepts/autograder/utils.py
+++ Course 3 - Advanced Computer Vision/Week 1 - Concepts/autograder/utils.py
@@ -2,7 +2,6 @@ from disable_warnings import *
 import PIL.Image, PIL.ImageFont, PIL.ImageDraw
 import numpy as np
 import tensorflow as tf
-from matplotlib import pyplot as plt
 import tensorflow_datasets as tfds
 from tools import send_feedback
 
